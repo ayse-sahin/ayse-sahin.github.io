74c59785bb327bb4dbc2f4f9cbc6c431a045942b
Bora <boraeroglu238@hotmail.com>
2024-10-23 21:57:37 +0200

    fix: services formatting

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,7 +6,7 @@ const Services = () => {
     const [services, setServices] = useState(null)
 
     const style = {
-        backgroundImage: 'url("src/assets/images/gr-background.png")',
+        backgroundImage: 'url("src/assets/images/watercolor3.jpeg")',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         position: 'relative'
@@ -23,7 +23,7 @@ const Services = () => {
         <section style={style} className="text-blue-950 pt-24 pb-10 bg-gray-100">
             <div className="container mx-auto px-6 text-center relative z-10">
                 <h2 className="text-3xl">Danışanlarım bana hangi sebeplerle gelir?</h2>
-                <div className="pt-8 mt-10 grid grid-cols-1 md:grid-cols-3 gap-8">
+                <div className="pt-8 mt-10 grid grid-cols-1 md:grid-cols-3 gap-2">
                     { services && services.items.map((service, index) => {
                         return <ServiceCard key={index} title={service.title} text={service.text}/>
                     }) }
