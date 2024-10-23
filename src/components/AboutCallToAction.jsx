7deb46288bd63c35074dd46be0d411fa5a944363
Bora <boraeroglu238@hotmail.com>
2024-10-23 23:52:05 +0200

    fix: about call-to-action

--- src/components/AboutCallToAction.jsx
+++ src/components/AboutCallToAction.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 
 const AboutCallToAction = () => {
     return (
-        <section className="py-16 bg-blue-950 px-6 text-white text-center">
-            <h3 className="text-3xl font-bold">Ready to Start Your Journey to Healing?</h3>
-            <p className="mt-4 text-lg">Book an appointment with Ayşe Şahin today and take the first step towards mental well-being.</p>
-            <Link onClick={() => { window.scroll(0, 0); }} to="/contact" className="transition-colors duration-500 mt-8 inline-block bg-white text-blue-950 px-8 py-3 rounded-lg hover:bg-gray-300">Book Appointment</Link>
+        <section className="py-20 bg-blue-950 px-6 text-white text-center">
+            <h3 className="text-3xl italic">Kendinizi Anlamak ve İyileşmek İçin Bir Yolculuğa Çıkın!</h3>
+            <p className="mt-4 text-lg">Hemen bir randevu oluşturun ve kendiniz için ilk adımı atın.</p>
+            <Link onClick={() => { window.scroll(0, 0); }} to="/contact" className="transition-colors duration-500 mt-8 inline-block bg-white text-blue-950 px-8 py-3 rounded-lg hover:bg-gray-300">Randevu Oluştur</Link>
         </section>
     )
 }
