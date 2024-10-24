fb40480e23d387e344e36f89ef013e5aaba3ea72
Bora <boraeroglu238@hotmail.com>
2024-10-24 21:40:28 +0200

    fix: acordion styling for faq

--- src/components/ContactHero.jsx
+++ src/components/ContactHero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ContactHero = () => {
     return (
-        <section className="hero-background pt-52 pb-20 md:py-32 relative text-white">
+        <section className="hero-background pt-52 pb-20 md:py-20 relative text-white">
             <div className="hero-overlay"></div>
             <div className="container relative z-10 px-8 mx-auto text-center">
                 <h1 className="text-4xl"></h1>
