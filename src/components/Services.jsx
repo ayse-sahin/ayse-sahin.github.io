6f6ca8ce7fa31c58d46a08346f0591746c5fbf86
Bora <boraeroglu238@hotmail.com>
2024-10-25 23:46:52 +0200

    fix: services background image

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,7 +5,7 @@ const Services = () => {
     const [services, setServices] = useState(null)
 
     const style = {
-        backgroundImage: 'url("src/assets/images/watercolor3.jpeg")',
+        backgroundImage: 'url("src/assets/images/bg-green.jpg")',
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         position: 'relative'
@@ -19,7 +19,7 @@ const Services = () => {
     }, [])
 
     return (
-        <section style={style} className="text-blue-950 pt-24 pb-14 bg-gray-100">
+        <section style={style} className="text-rose-950 pt-24 pb-14 bg-gray-100">
             <div className="container mx-auto px-6 text-center relative z-10">
                 <h2 className="text-4xl">Size nasıl yardım edebilirim?</h2>
                 <div className="pt-8 mt-10 grid grid-cols-1 md:grid-cols-3 gap-x-2 gap-y-12">
