7677b0639237525e6a7aa0e9a1a0518911d021f3
Bora <yusuf.bora.eroglu@gmail.com>
2025-02-08 16:35:52 +0300

    fix: change router type back for custom domain routing, first pskaysesahin.com build

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter as HashRouter, Routes, Route } from 'react-router-dom'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
 import React from 'react'
 import HomePage from './pages/HomePage'
 import MainLayout from './layouts/MainLayout'
@@ -9,21 +9,23 @@ import NotFound from './pages/NotFound'
 import ContactPage from './pages/ContactPage'
 import FAQPage from './pages/FAQPage'
 
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<MainLayout />}>
+            <Route index element={<HomePage />}/>
+            <Route path='/about' element={<AboutPage />}/>
+            <Route path='/blogs' element={<BlogsPage />}/>
+            <Route path='/contact' element={<ContactPage />}/>
+            <Route path='/faq' element={<FAQPage />}/>
+            <Route path='/blogs/:id' element={<SingleBlogPage />}/>
+            <Route path='*' element={<NotFound />}/>
+        </Route>
+    )
+)
+
 const App = () => {
     return (
-        <HashRouter>
-            <Routes>
-                <Route path='/' element={<MainLayout />}>
-                    <Route index element={<HomePage />}/>
-                    <Route path='/about' element={<AboutPage />}/>
-                    <Route path='/blogs' element={<BlogsPage />}/>
-                    <Route path='/contact' element={<ContactPage />}/>
-                    <Route path='/faq' element={<FAQPage />}/>
-                    <Route path='/blogs/:id' element={<SingleBlogPage />}/>
-                    <Route path='*' element={<NotFound />}/>
-                </Route>
-            </Routes>
-        </HashRouter>
+        <RouterProvider router={router}/>
     )
 }
 
