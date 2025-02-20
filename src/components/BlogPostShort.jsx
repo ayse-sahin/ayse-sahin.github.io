2b3f42b2ae45d1bcbc685d3c78b95c4da7760f71
Bora <yusuf.bora.eroglu@gmail.com>
2025-02-20 20:18:01 +0300

    blog cards link user to the blog wherever you click on them

--- src/components/BlogPostShort.jsx
+++ src/components/BlogPostShort.jsx
@@ -8,7 +8,7 @@ const BlogPostShort = ({ blog, urlPrefix = '' }) => {
     }
 
     return (
-        <div className="grid grid-cols-1 shadow-xl rounded-lg p-4">
+        <Link onClick={() => { window.scroll(0, 0); }} to={`/blogs/${blog.id}`} className="grid grid-cols-1 shadow-xl rounded-lg p-4">
             <div style={style} className='flex w-full rounded-t-md h-24 mb-4 justify-center bg-cover'> </div>
             <h3 className="text-xl font-serif-override font-bold text-dark-green">{blog.title}</h3>
             <div className="mt-2 text-justify text-dark-green">
@@ -16,7 +16,7 @@ const BlogPostShort = ({ blog, urlPrefix = '' }) => {
                     {blog.description}
                 </ReactMarkdown></div>
             <Link onClick={() => { window.scroll(0, 0); }} to={`/blogs/${blog.id}`} className="self-end mt-4 inline-block text-link-hover hover:underline">Devamını Oku</Link>
-        </div>
+        </Link>
     )
 }
 
